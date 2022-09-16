65c06cc60fff82a9c1362aa6b30d6583d59fc7c8
Antoine-Mtx <antoine.mortureux@gmail.com>
2022-09-16 15:30:26 +0200

    Rails 7: The Demo tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ test/mailers/previews/comments_mailer_preview.rb
@@ -0,0 +1,11 @@
+# Preview all emails at http://localhost:3000/rails/mailers/comments_mailer
+class CommentsMailerPreview < ActionMailer::Preview
+
+  p Comment.first
+
+  # Preview this email at http://localhost:3000/rails/mailers/comments_mailer/submitted
+  def submitted
+    CommentsMailer.submitted Comment.first
+  end
+
+end
